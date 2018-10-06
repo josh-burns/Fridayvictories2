6f8c82da37e91647d4bafd5598bd8b6dc3090c89
Joshua Burns <josh.burns1993@gmail.com>
2018-10-06 11:57:34 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# Friday Victories 
+
+A tool designed for companies to receive and broadcast staff-members' responses to the following questions: 
+
+* "*What is your **professional** victory this week?*"
+* "*What is your **personal** victory this week?*"
+
+
+#Installation 
+
+
+####Requirements 
+
+This tool uses the following libraries: 
+* Gspread 
+* Oath2Client
+* Sys 
+* Time
+
+A client_secret.json file is required for the Google Sheets integration ([See more here](https://developers.google.com/sheets/api/guides/authorizing)), with the following information:
+
+```json
+{
+  "type": "",
+  "project_id": "",
+  "private_key_id": "",
+  "private_key": "",
+  "client_email": "",
+  "client_id": "",
+  "auth_uri": "",
+  "token_uri": "",
+  "auth_provider_x509_cert_url": "",
+  "client_x509_cert_url": ""
+}
+
+```
+
+A keys.py file is required for the Slack integration, with the following information:
+
+```
+token = "TOKEN-RECIEVED-FROM-SLACK"
+
+clientSecret = 'notasecret' 
+```
+
+
+
+### To Do 
+
+* Sanitise email address to remove domain to just  have "Josh" instead of "josh@company.co"
+* Add unit tests and error handling
+
---
+++ fridayVictories.py
@@ -0,0 +1,123 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import sys
+from slackclient import SlackClient
+from keys import (token, clientSecret)
+import time
+
+# Any print() will be written to the HTML file
+sys.stdout = open('file.html', 'w')
+
+print("<link href=\"https://fonts.googleapis.com/css?family=Lato\" rel=\"stylesheet\"> ")
+print("<style> body { background-image: url(\"https://www.goodlord.co/images/hp_burst.jpg\"); "
+      "font: normal 15px Verdana, Arial, sans-serif; font-family:'Lato'; color: black} "
+      ".response {background-color: #bfbfbf; border-radius: 15px 50px 15px; z-index: 2; "
+      "max-width:700px; padding:40px; opacity:80%; "
+      "margin: 15 auto; display:flex; opacity: 30%;}"
+      ".title {width 300px; margin: 30 auto; color: #d8d8d8; border-radius: 15px 50px 15px; "
+      "max-width:700px; padding:50px; "
+      "opacity:80%; margin: 15 auto; font-family:'Lato'; background-color: grey;}"
+      ".photo{ width: 192px; display:inline-block; order: 1; margin: 60px;}"
+      ".text_body {order: 2; margin: 10 auto; background-color: #d8d8d8; "
+      "border-radius: 15px 50px 15px; max-width:700px; padding: 30px;}"
+      ".title_text {vertical-align:center; text-align: center;}"
+      "</style>"
+      "<div class=\"title\"> <h1> <div class=\"title_text\"> Friday Victories </h1> </div> </div>"
+      )
+
+
+def html_work_and_personal_vics():
+    print("<i><b> Work Victory:</b> </i> <br>")
+    print(workVictory[count], "<br> <br>")
+    print("<i><b> Personal Victory: </b> </i> <br>")
+    print(persVictory[count], "</div>")
+    print("</div>", '</div>')
+
+
+def print_slack_photo_and_real_name():
+    # api call to slack to obtain user info, based on email address of responder
+    userPhoto.insert(0, sc.api_call("users.lookupByEmail", email=emailAddress[count]))
+    print("<div class=\"response\">")
+    print("<div class = \"photo\" >", "<img src= \" ", userPhoto[0]['user']['profile']['image_192'], "\">",
+          '</div>' "<br>")
+    print("<div class=\"text_body\">", "<b> <h2> ", userPhoto[0]['user']['real_name'], " </b> </h2>")
+
+
+def print_dog_picture_and_email_address():
+    userPhoto.insert(0, "https://cdn.shopify.com/s/files/1/1429/4170/articles/Four_easy_dog_calming_tips_Cropped_1_a6703117-eb49-49bc-8b27-b2a73c599299_grande.jpg?v=1516848931")
+    print("<div class=\"response\">")
+    print("<div class = \"photo\" >", "<img src= \" ", userPhoto[0], "\">", '</div>' "<br>")
+    print("<div class=\"text_body\">", "<b>", emailAddress[count], "</b>", "<br>", "<br>")
+
+
+# Google forms/sheets API/WebHook integration
+CLIENT_SECRET = clientSecret
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+client = gspread.authorize(creds)
+client = gspread.authorize(creds)
+sheet = client.open('AutomatedFridayVictoriesResponses').sheet1
+
+
+# slack token
+slack_token = token
+sc = SlackClient(slack_token)
+slackUsers = sc.api_call("users.list")
+
+# val checks if the timestamp field of the response spreadsheet is populated or not, to check whether to progress
+val: str = sheet.acell('A2').value
+emailAddress = list()
+userPhoto = []
+workVictory = []
+persVictory = []
+displayName =[]
+responsesIngested = 0
+count = 0
+
+# write responses to list, and remove each row in spreadsheet so it is ready for next use
+while val is not "":
+    val = sheet.acell('a2').value
+    emailAddress.append(sheet.acell('b2').value)
+    workVictory.append(sheet.acell('c2').value)
+    persVictory.append(sheet.acell('d2').value)
+    sheet.delete_row(2)
+    time.sleep(1)
+    count += 1
+
+else:
+    responsesIngested += 1
+    emailAddress = emailAddress[:-1]
+    count -= 2
+
+# populate a list with slack photos corresponding to each email address
+
+while responsesIngested is not 0:
+    while count > 0:
+        try:
+            print_slack_photo_and_real_name()
+            html_work_and_personal_vics()
+            count -= 1
+
+        except KeyError or TypeError:
+            print_dog_picture_and_email_address()
+            html_work_and_personal_vics()
+            count -= 1
+    else:
+        try:
+            print_slack_photo_and_real_name()
+            html_work_and_personal_vics()
+            count -= 1
+            responsesIngested -= 1
+
+        except KeyError or TypeError:
+            print_dog_picture_and_email_address()
+            html_work_and_personal_vics()
+            count -= 1
+            responsesIngested -= 1
+
+    print("<div class=\"title\">", "<h1> <div class=\"title_text\">", "This week,", (len(workVictory) - 1),
+          " people filled in their Friday victories!", "</div>", "</div>")
+
+
+
